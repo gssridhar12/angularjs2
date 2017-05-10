70aa18fec15b8365b252056d26f27624026fb7c2
Sridhar <sridhar.cse12@gmail.com>
2017-05-11 01:54:41 +0530

    directives ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+  serverId = Math.random() * 10;
+  serverStatus = this.serverId>5 ? "online" : "offline";
 
+  getColor(){
+    return this.serverId > 5 ? "green" : "red";
+  }
 }
